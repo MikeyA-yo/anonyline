8ef8aefbe4eae503a176517155d4afb26ccfb3b3
Ayomide Oluwatola <ayomideoluwatola1@gmail.com>
2025-01-29 16:11:38 +0100

    mobile nav loading...

--- components/nav.tsx
+++ components/nav.tsx
@@ -1,9 +1,14 @@
+"use client";
 import Link from "next/link";
+import { useState } from "react";
+import { HiBars3BottomRight } from "react-icons/hi2";
+import { MdClose } from "react-icons/md";
 
 export default function Nav(){
+    const [open, setOpen] = useState(false);
     return (
         <>
-          <nav className="backdrop-blur-md lg:flex md:flex hidden items-center justify-evenly w-full fixed p-5 text-white">
+          <nav className="backdrop-blur-md lg:flex md:flex hidden items-center shadow-sm shadow-[#7A1CAC] justify-evenly w-full fixed p-5 text-white">
             <Link href={"/"} className="text-xl font-bold">AnonyLine..</Link>
             <div className="p-3 border-[0.5px] border-[#FFFBFF] bg-[#FBFFFF] bg-opacity-10 backdrop-blur-md rounded-full flex gap-3">
                 <Link href={"/about"}>About</Link>
@@ -12,6 +17,25 @@ export default function Nav(){
             </div>
             <Link href={"/login"}>Login</Link>
           </nav>
+          <nav className="lg:hidden md:hidden fixed w-full flex items-center text-white p-3 backdrop-blur-md justify-between">
+            <Link href={"/"} className="text-xl font-bold">AnonyLine..</Link>
+            <Bar open={open} setOpen={setOpen} />
+          </nav>
+        </>
+    )
+}
+function SideBar({}){
+    return (
+        <></>
+    )
+}
+function Bar({open, setOpen}:{open:boolean, setOpen:React.Dispatch<React.SetStateAction<boolean>>}){
+    return (
+        <>
+        {!open && <HiBars3BottomRight className="fill-[#EBD3F8] h-8 w-8" onClick={()=>{
+            setOpen(true)
+        }}/>}
+        {open && <MdClose className="fill-[#EBD3F8] h-8 w-8" onClick={()=>{ setOpen(false)}} />}
         </>
     )
 }
