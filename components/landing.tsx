df4c3cde9e607991ce56a30789bfa23be725f797
Ayomide Oluwatola <ayomideoluwatola1@gmail.com>
2025-01-30 10:18:56 +0100

    idk what to do

--- components/landing.tsx
+++ components/landing.tsx
@@ -14,8 +14,10 @@ export default function Landing() {
 function Hero() {
   return (
     <>
-      <div className="bg-gradient-to-b from-[#2E073F] to-[#7A1CAC] h-[95svh]">
-        Yoo
+      <div className="bg-gradient-to-b text-center px-4 flex flex-col items-center justify-center gap-5 from-[#2E073F] to-[#7A1CAC] h-[100svh]">
+        <div>
+            <h2 className="lg:text-3xl text-2xl bg-gradient-to-r font-extrabold from-[#AD49E1] to-[#EBD3F8] text-transparent bg-clip-text">Enjoy seamless anonymous chat with Anonyline</h2>
+        </div>
       </div>
     </>
   );
