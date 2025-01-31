2df8825a6836b024a8b94d01b469c4c9e00e0ac2
Ayomide Oluwatola <ayomideoluwatola1@gmail.com>
2025-01-31 19:20:07 +0100

    na i quit

--- components/parallax.tsx
+++ components/parallax.tsx
@@ -10,33 +10,33 @@ export default function Parallax({ children }: { children?: React.ReactNode }) {
   });
 
   // Use pixel values instead of percentages
-  const y1 = useTransform(scrollYProgress, [0, 1], ["0", "-250%"]);
-  const y2 = useTransform(scrollYProgress, [0, 1], ["0", "-500%"]);
-  const y3 = useTransform(scrollYProgress, [0, 1], ["0", "-750%"]);
+  const y1 = useTransform(scrollYProgress, [0, 1], ["0", "-10%"]);
+  const y2 = useTransform(scrollYProgress, [0, 1], ["0", "-100%"]);
+  const y3 = useTransform(scrollYProgress, [0, 1], ["0", "-150%"]);
 
   return (
-    <div className="relative h-[300vh] overflow-hidden" ref={ref}>
+    <div className="relative h-[300vh] " ref={ref}>
       <div className="sticky top-0 h-screen flex items-center justify-center bg-neutral-900">
         {/* Use motion.div directly */}
         <motion.div 
-          className="absolute text-4xl text-white z-30"
+          className="absolute text-4xl text-white z-20"
           style={{ y: y1 }}
         >
-          <p>First Layer</p>
+          <p>Created for the purpose of back and forth 2 or multi way communication, rather than Just a one way mode</p>
         </motion.div>
         
         <motion.div 
-          className="absolute text-4xl text-white z-20"
+          className="absolute text-4xl text-white z-30"
           style={{ y: y2 }}
         >
-          <p>Second Layer</p>
+          <p>A simple Chat is all it is, but with the purpose of anonymity to allow users feel as free</p>
         </motion.div>
         
         <motion.div 
-          className="absolute text-4xl text-white z-10"
+          className="absolute text-4xl text-white z-40"
           style={{ y: y3 }}
         >
-          <p>Third Layer</p>
+          <p>Enhanced features for multi way chatting such as the concept of a room</p>
         </motion.div>
         
         {children}
