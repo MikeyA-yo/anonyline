6dbebbc73ff1784eecbde3619b73958e2b903b43
Ayomide Oluwatola <ayomideoluwatola1@gmail.com>
2025-01-31 13:03:13 +0100

    na so i don't know what to write in about

--- components/about.tsx
+++ components/about.tsx
@@ -1,8 +1,13 @@
+import { Roboto } from "next/font/google"
+
+const rob = Roboto({weight:["500"], style:["normal"], subsets:["latin"]})
 export default function About(){
     return (
         <>
           <div className="h-[50vh] text-[#EBD3F8] flex flex-col items-center justify-center">
-            <div className="z-10">Yoo</div>
+            <div className="z-10">
+              <h3 className={`text-2xl font-bold ${rob.className}`}>What is Anonyline?</h3>
+            </div>
           </div>
         </>
     )
