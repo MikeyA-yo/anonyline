684eb3b80fcdbe4313886f0827fba45b987b2b33
Ayomide Oluwatola <ayomideoluwatola1@gmail.com>
2025-02-05 18:00:50 +0100

    i love when my code works on first try

---
+++ app/api/sendmsg/route.ts
@@ -0,0 +1,36 @@
+import { NextResponse } from "next/server";
+import { createTransport } from "nodemailer";
+import Mail from "nodemailer/lib/mailer";
+
+const transport = createTransport({
+  service: "gmail",
+  auth: {
+    user: process.env.GMAIL,
+    pass: process.env.GMAIL_PASS,
+  },
+});
+
+export async function POST(req: Request, res: Response) {
+  const { name, email, phone, message } = await req.json();
+  const mailOpts: Mail.Options = {
+    from: process.env.GMAIL,
+    to: process.env.GMAIL,
+    html: `<p>Name: ${name}\nEmail: ${email}\nPhone: ${phone}\nMessage: ${message}</p>`,
+  };
+  const sendMailPromise = () =>
+    new Promise<string>((resolve, reject) => {
+      transport.sendMail(mailOpts, function (err) {
+        if (!err) {
+          resolve("Email sent");
+        } else {
+          reject(err.message);
+        }
+      });
+    });
+  try {
+    await sendMailPromise();
+    return NextResponse.json({ message: "Email sent" });
+  } catch (e) {
+    return NextResponse.json({ message: "Email not sent" });
+  }
+}
--- components/contactform.tsx
+++ components/contactform.tsx
@@ -1,5 +1,38 @@
 "use client";
+
+import { useCallback, useReducer, useState } from "react";
+function reducer(state: {name:string, email:string, phone:string, message:string}, action:{type:string, value:string}){
+    return {...state, [action.type]:action.value};
+}
 export default function ContactForm(){
+    const [state, dp] = useReducer(reducer, {
+        name:"",
+        email:"",
+        phone:"",
+        message:"",
+    });
+    const [loading, setLoading] = useState(false);
+    const [error, setError] = useState(false);
+    const [success, setSuccess] = useState(false);
+    const sendMsg = useCallback(async ()=>{
+        setLoading(true)
+        const res = await fetch ("/api/sendmsg", {
+            method:"POST",
+            body:JSON.stringify(state),
+            headers:{
+                "Content-Type":"application/json"
+            }
+        })
+        setLoading(false);
+        if (!res.ok) setError(true);
+        const jsn = await res.json();
+        if (res.ok && jsn.message === "Email sent") {
+            setSuccess(true);
+            return
+        }
+        setError(true)
+    }, [state]);
+    console.log(loading, error, success)
     return (
         <>
           <div className="flex flex-col gap-4 p-5 items-center bg-[#2E073F] shadow-md shadow-[#EBD3F8] lg:min-h-96 md:min-h-80 md:w-[90%] justify-self-end">
@@ -7,26 +40,36 @@ export default function ContactForm(){
             <InputGrps>
               <InputContainer>
                 <label htmlFor="name">Name</label>
-                <input id="name" placeholder="Please enter your name"  className="p-2 rounded-md text-[#2E073F]"/>
+                <input onChange={(e)=>{
+                    dp({type:"name", value:e.target.value})
+                }} id="name" placeholder="Please enter your name"  className="p-2 rounded-md text-[#2E073F]"/>
               </InputContainer>
               <InputContainer>
                 <label htmlFor="email">Email</label>
-                <input id="email" type="email" placeholder="Please enter your email" className="p-2 rounded-md text-[#2E073F]" />
+                <input onChange={(e)=>{
+                    dp({type:"email", value:e.target.value})
+                }} id="email" type="email" placeholder="Please enter your email" className="p-2 rounded-md text-[#2E073F]" />
               </InputContainer>
             </InputGrps>
             <InputGrps>
               <InputContainer>
                 <label htmlFor="Phone">Phone</label>
-                <input id="Phone" type="tel" placeholder="Please enter your phone number" className="p-2 rounded-md text-[#2E073F]" />
+                <input onChange={(e)=>{
+                    dp({type:"phone", value:e.target.value})
+                }} id="Phone" type="tel" placeholder="Please enter your phone number" className="p-2 rounded-md text-[#2E073F]" />
               </InputContainer>
             </InputGrps>
             <InputGrps>
               <InputContainer>
                 <label htmlFor="msg">Message</label>
-                <textarea id="msg" placeholder="Please enter your message" className="p-2 rounded-md text-[#2E073F] min-h-20" />
+                <textarea onChange={(e)=>{
+                    dp({type:"message", value:e.target.value})
+                }} id="msg" placeholder="Please enter your message" className="p-2 rounded-md text-[#2E073F] min-h-20" />
               </InputContainer>
             </InputGrps>
-            <button className="self-end px-2 py-1 bg-[#7A1CAC] rounded">Send</button>
+            <button className="self-end px-2 py-1 bg-[#7A1CAC] rounded" onClick={()=>{
+                sendMsg()
+            }}>Send</button>
           </div>
         </>
     )
