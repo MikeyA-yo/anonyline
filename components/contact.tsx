2d323d73ac1359832c6b30100dc3073624c878ae
Ayomide Oluwatola <ayomideoluwatola1@gmail.com>
2025-02-05 14:24:28 +0100

    first section in contact done

--- components/contact.tsx
+++ components/contact.tsx
@@ -1,15 +1,37 @@
-import { Inter } from "next/font/google"
+import { Inter, Roboto_Mono } from "next/font/google"
+import ContactForm from "./contactform"
+import { MdOutlineEmail, MdOutlinePhone } from "react-icons/md"
+import { FaGithub } from "react-icons/fa";
+import "@/components/css/contact.css";
 
 const inter = Inter({weight:["400", "700"], subsets:["latin"]})
+const robMono = Roboto_Mono({weight:["400", "700"], subsets:["latin"]});
 export default function Contact(){
     return (
         <>
-          <div className="min-h-[50vh] flex flex-col text-[#EBD3F8] items-center justify-center" id="contact">
+          <div className="min-h-[50vh] flex flex-col text-[#EBD3F8] items-center justify-center p-10 gap-5" id="contact">
             <div className="z-10">
                 <h3 className={`${inter.className} text-3xl font-bold `}>Contact</h3>
             </div>
-            <div className="grid lg:grid-cols-2 md:grid-cols-2 grid-cols-1">
-                <div></div>
+            <div className="grid lg:grid-cols-2 md:grid-cols-2 grid-cols-1 z-10">
+                <div className={`lg:grid-cols-3 grid grid-cols-2 gap-4 ${robMono.className}`}>
+                    <div className="h-52 w-52 bg-[#7A1CAC] shadow-md shadow-[#EBD3F8] flex flex-col items-center hyphens-manual gap-4 p-5 relative hover:animate-moving-border">
+                      <MdOutlineEmail className="h-10 w-10 text-[#EBD3F8]" />
+                      <h4 className={`text-lg font-bold`}>Email</h4>
+                      <a href="mailto:ayomideoluwatola1@gmail.com" className="text-sm">ayomideoluwatola1&shy;@gmail.com</a>
+                    </div>
+                    <div className="h-52 w-52 bg-[#7A1CAC] shadow-md shadow-[#EBD3F8] flex flex-col items-center gap-4 p-5">
+                       <MdOutlinePhone className="h-10 w-10 text-[#EBD3F8]" />
+                       <h4 className={`text-lg font-bold`}>Phone</h4>
+                       <a href="https://wa.me/2348089132385" className="text-sm">+234 808 913 2385</a>
+                    </div>
+                    <div className="h-52 w-52 bg-[#7A1CAC] shadow-md shadow-[#EBD3F8] flex flex-col items-center gap-4 p-5">
+                      <FaGithub className="h-10 w-10 text-[#EBD3F8]" />
+                      <h4 className={`text-lg font-bold`}>Github</h4>
+                      <a href="https://github.com/MikeyA-yo" className="text-sm">MikeyA-yo</a>
+                    </div>
+                </div>
+                <ContactForm />
             </div>
           </div>
         </>
---
+++ components/contactform.tsx
@@ -0,0 +1,8 @@
+"use client";
+export default function ContactForm(){
+    return (
+        <>
+          <div></div>
+        </>
+    )
+}
