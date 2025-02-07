c1deec567d49b65e548d08e464de3d5708a9ac2f
Ayomide Oluwatola <ayomideoluwatola1@gmail.com>
2025-02-07 16:05:29 +0100

    basic appwrite things

--- components/login.tsx
+++ components/login.tsx
@@ -1,5 +1,11 @@
 export default function Login(){
     return (
-        <></>
+        <>
+         <div className="min-h-100svh grid lg:grid-cols-2 md:grid-cols-2 grid-cols-1 bg-[#2E073F]">
+            <div></div>
+            <div className="flex flex-col items-center justify-center p-5"></div>
+         </div>
+        </>
     )
 }
+
---
+++ components/user.ts
@@ -0,0 +1,14 @@
+import { account, ID } from "@/app/appwrite_config/appwriteConfig";
+import { hash } from "bcrypt";
+
+export async function createUser( email:string, password:string, name?:string){
+    const pwd = await hash(password, 10);
+    const result = await account.create(ID.unique(), email, pwd, name);
+    return result;
+}
+
+export async function loginUser(email:string, password:string){
+    const pwd = await hash(password, 10);
+    const session = await account.createEmailPasswordSession(email, pwd);
+    return session
+}
