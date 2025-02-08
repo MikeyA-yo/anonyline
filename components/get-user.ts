a7e6f2b7c61de8a0703378bc53452e23c605db6d
Ayomide Oluwatola <ayomideoluwatola1@gmail.com>
2025-02-08 20:23:14 +0100

    mmm:

--- components/get-user.ts
+++ components/get-user.ts
@@ -1,5 +1,5 @@
 import { account } from "@/app/appwrite_config/appwriteConfig";
-async function getUser (){
+export async function getUser (){
   const user = await account.get();
   return user;
 }
