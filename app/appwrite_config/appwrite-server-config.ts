34a1f5c429c716cdc188488e7e02baefadd9c6c3
Ayomide Oluwatola <ayomideoluwatola1@gmail.com>
2025-02-09 18:40:02 +0100

    make appwrite server config

---
+++ app/appwrite_config/appwrite-server-config.ts
@@ -0,0 +1,19 @@
+"use server";
+import { Client, Account } from "node-appwrite";
+import { cookies } from "next/headers";
+
+export async function createSessionClient(){
+    const client = new Client().setEndpoint(process.env.APPWRITE_URL as string).setProject(process.env.APPWRITE_ID as string);
+    const session = (await cookies()).get("user");
+  if (!session || !session.value) {
+    throw new Error("No session");
+  }
+
+  client.setSession(session.value);
+
+  return {
+    get account() {
+      return new Account(client);
+    },
+  };
+}
