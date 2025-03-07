ea61dc0f9f66ad1b7dc62b11ec49313508ffb5d6
Ayomide Oluwatola <ayomideoluwatola1@gmail.com>
2025-03-07 20:13:42 +0100

    in a last effort to defeat ... omo

--- middleware.ts
+++ middleware.ts
@@ -3,13 +3,13 @@ import type { NextRequest } from 'next/server'
 import { getSafeUser } from './components/getUser'
 
 export async function middleware(request: NextRequest) {
-  const authCookie = await getSafeUser()
-  // const { pathname } = request.nextUrl
+//   const authCookie = await getSafeUser()
+//   // const { pathname } = request.nextUrl
 
-  if (!authCookie){
-    console.log(authCookie)
-    return NextResponse.redirect(new URL('/login', request.url))
-  }
+//   if (!authCookie){
+//     console.log(authCookie)
+//     return NextResponse.redirect(new URL('/login', request.url))
+//   }
 
   return NextResponse.next()
 }
