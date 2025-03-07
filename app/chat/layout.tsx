c16c540f3e5a74f7c8d4c0ac792a9170771ded1c
Ayomide Oluwatola <ayomideoluwatola1@gmail.com>
2025-03-07 19:15:10 +0100

    seems i cooked myself -- yup i did

--- app/chat/layout.tsx
+++ app/chat/layout.tsx
@@ -1,20 +1,20 @@
 import SideBar from "@/components/chat-sidebar";
-import { redirect } from "next/navigation"
-import { getSafeUser } from "@/components/getUser"
+import { redirect } from "next/navigation";
+import { getSafeUser } from "@/components/getUser";
 import { rooms } from "@/components/db";
 import { Metadata } from "next";
-export const metadata:Metadata = {
+export const metadata: Metadata = {
   title: "Chat",
-  description:"Chat with people"
-}
-export default  async function ChatLayout({
+  description: "Chat with people",
+};
+export default async function ChatLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
   const user = await getSafeUser();
-   if (!user) return redirect("/login");
-   const col = await rooms(user);
+  if (!user) return redirect("/login");
+  const col = await rooms(user);
   return (
     <main className="flex">
       <SideBar rooms={col} />
