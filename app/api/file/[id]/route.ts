b2198af86acdbaf2deb1460099122afed5916005
Ayomide Oluwatola <ayomideoluwatola1@gmail.com>
2025-03-07 18:40:57 +0100

    my name is Ayo, and nextjs you can go fuck urself

--- app/api/file/[id]/route.ts
+++ app/api/file/[id]/route.ts
@@ -3,14 +3,13 @@ import { NextRequest } from "next/server";
 
 export async function GET(
   _: NextRequest,
-  { params }: { params: { id: string } }
+  { params }: { params: Promise<{ id: string }> }
 ) {
-  const fileId = params.id;
-  const { storage } = await createSessionStorage();
+  const fileId = (await params).id;
+  const {storage} = await createSessionStorage();
   const file = await storage.getFileView(process.env.PFP as string, fileId);
   const fileU8 = new Uint8Array(file);
   const fileArr = Array.from(fileU8);
-  
   // Now you can use fileId in your logic
   return Response.json({ id: fileId, file: fileArr });
 }
