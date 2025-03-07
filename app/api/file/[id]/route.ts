6dda3efb00132292f30413089264e398940558c1
Ayomide Oluwatola <ayomideoluwatola1@gmail.com>
2025-03-07 18:36:24 +0100

    if this doesn't work...

--- app/api/file/[id]/route.ts
+++ app/api/file/[id]/route.ts
@@ -2,14 +2,15 @@ import { createSessionStorage } from "@/app/appwrite_config/appwrite-server-conf
 import { NextRequest } from "next/server";
 
 export async function GET(
-  _:NextRequest,
-  params: Promise<{ id: string }>
+  _: NextRequest,
+  { params }: { params: { id: string } }
 ) {
-  const fileId = (await params).id;
-  const {storage} = await createSessionStorage();
+  const fileId = params.id;
+  const { storage } = await createSessionStorage();
   const file = await storage.getFileView(process.env.PFP as string, fileId);
   const fileU8 = new Uint8Array(file);
   const fileArr = Array.from(fileU8);
+  
   // Now you can use fileId in your logic
   return Response.json({ id: fileId, file: fileArr });
 }
