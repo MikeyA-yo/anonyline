d4cafcec336f76b3ed581abb222fc5bc48792bb7
Ayomide Oluwatola <ayomideoluwatola1@gmail.com>
2025-03-31 23:16:10 +0100

    since i am doing email validation, then would optimize image soon

--- components/login-actions.ts
+++ components/login-actions.ts
@@ -16,49 +16,66 @@ export async function CreateLogin(formState: any, formData: FormData) {
     };
   }
   const { email, password } = data.data;
-  const {error} = await loginUser(email, password);
-  if (error){
+  const { error } = await loginUser(email, password);
+  if (error) {
     return {
-      errors: error.message
-    }
+      errors: error.message,
+    };
   }
   redirect("/chat");
 }
 // eslint-disable-next-line @typescript-eslint/no-explicit-any
 export async function CreateAcct(formState: any, formData: FormData) {
-  try {const pwd = formData.get("password")?.toString();
+  try {
+    const pwd = formData.get("password")?.toString();
     const em = formData.get("email")?.toString();
     const l = formData.get("lname")?.toString();
     const f = formData.get("fname")?.toString();
-    const nm = mix(l, f)
-    const data = loginSChema.safeParse({password:pwd, email:em, name:nm});
+    const nm = mix(l, f);
+    const data = loginSChema.safeParse({ password: pwd, email: em, name: nm });
     if (!data.success) {
       console.log(data.error.errors);
       return {
         errors: data.error.flatten().fieldErrors,
       };
     }
-    const {name, email, password} = data.data;
-    const res = await createUser(email, password, name);
-    if (res){
+    const { name, email, password } = data.data;
+    const emailValidationResponse = await fetch(
+      `https://emailvalidation.abstractapi.com/v1/?api_key=${process.env.ABSTRACT_API}&email=${email}`
+    );
+    const emailValidationResult = await emailValidationResponse.json();
+    if (!emailValidationResult.is_valid_format.value) {
       return {
-          success: "Successful check your email: " + email + " for verification, if you don't see it check spam folder"
-      }
-    }} catch (e){
-      if (e instanceof Error)
-        {return {
-            errors: e.message
-        }}
+        errors: "Invalid email format. Please enter a valid email address.",
+      };
     }
+    const res = await createUser(email, password, name);
+    if (res) {
+      return {
+        success:
+          "Successful check your email: " +
+          email +
+          " for verification, if you don't see it check spam folder",
+      };
+    }
+  } catch (e) {
+    if (e instanceof Error) {
+      return {
+        errors: e.message,
+      };
+    }
+  }
 }
 
-function mix(a?:string, b?:string){
-    let res = "";
-    if (!a || !b){
-        return ID.unique();
-    }
-    for (let i = 0; i < a.length; i++){
-        res += a[Math.floor(Math.random()*a.length)] + b[Math.floor(Math.random()*b.length)];
-    }
-    return res;
+function mix(a?: string, b?: string) {
+  let res = "";
+  if (!a || !b) {
+    return ID.unique();
+  }
+  for (let i = 0; i < a.length; i++) {
+    res +=
+      a[Math.floor(Math.random() * a.length)] +
+      b[Math.floor(Math.random() * b.length)];
+  }
+  return res;
 }
