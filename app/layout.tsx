b44e4202885768ebad8a4a2bba7560e1e6575ab8
Ayomide Oluwatola <ayomideoluwatola1@gmail.com>
2025-05-01 19:02:07 +0100

    SEO

--- app/layout.tsx
+++ app/layout.tsx
@@ -15,9 +15,31 @@ import "./globals.css";
 export const metadata: Metadata = {
   title: {
     template:"%s | Anonyline",
-    default:"Anonyline..",
+    default:"Anonyline",
   },
   description: "Anonyline is a very simple anonymous chat application",
+  keywords: ["anonymous chat", "anonymous messaging", "chat app", "anonymous forum", "free chat web app", "anonyline", "anonymous talks"],
+  openGraph: {
+    title: "Anonyline - Anonymous Chat Application",
+    description: "Anonyline is a platform for anonymous chatting and sharing thoughts",
+    url: "https://anonyline.vercel.app",
+    siteName: "Anonyline",
+    images: [
+      {
+        url: "https://anonyline.vercel.app/anonymous-1.png",
+        width: 1200,
+        height: 630,
+      },
+    ],
+    locale: "en_US",
+    type: "website",
+  },
+  twitter: {
+    card: "summary_large_image",
+    title: "Anonyline - Anonymous Chat Application",
+    description: "Anonyline is a platform for anonymous chatting and sharing thoughts",
+    images: ["https://anonyline.vercel.app/anonymous-1.png"],
+  },
 };
 
 export default function RootLayout({
